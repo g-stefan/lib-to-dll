9c25c4dc0dde4d49646ca51d23035044f787b50f
Grigore Stefan <g_stefan@yahoo.com>
2021-07-24 19:34:45 +0300

    update

--- source/lib-to-dll-version.hpp
+++ source/lib-to-dll-version.hpp
@@ -10,10 +10,10 @@
 #ifndef LIB_TO_DLL_VERSION_HPP
 #define LIB_TO_DLL_VERSION_HPP
 
-#define LIB_TO_DLL_VERSION_ABCD                1,4,0,7
-#define LIB_TO_DLL_VERSION_STR                 "1.4.0"
-#define LIB_TO_DLL_VERSION_STR_BUILD           "7"
-#define LIB_TO_DLL_VERSION_STR_DATETIME        "2021-07-06 15:11:51"
+#define LIB_TO_DLL_VERSION_ABCD                1,5,0,9
+#define LIB_TO_DLL_VERSION_STR                 "1.5.0"
+#define LIB_TO_DLL_VERSION_STR_BUILD           "9"
+#define LIB_TO_DLL_VERSION_STR_DATETIME        "2021-07-23 16:34:04"
 
 #ifndef XYO_RC
 
